c891e18e941a38bb58d656a628401e11f1d86ab8
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 15:42:27 +0200

    Add exo dollars américain

--- src/fr/github/quenk/helloworld/Main.java
+++ src/fr/github/quenk/helloworld/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
         //Cette ligne permet d'afficher: Hello World!
         System.out.println("Hello World!");
+
+        //Dollars américain: 1.22 // don: 1500€
+
+        double dollarsame = 1.22;
+        int somme= 1500;
+        float resultat = (float) (dollarsame * somme);
+        System.out.println(resultat);
     }
 }
